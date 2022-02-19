57447b1a94d972777bdf5e245a38a919c028f612
Bruno Cunha <brunocunha@Brunos-MacBook-Air.local>
2022-02-19 13:10:22 -0300

    meu primeiro commit que orgulho

---
+++ c_sandbox/aff_c.c
@@ -0,0 +1,28 @@
+#include <unistd.h>
+
+void aff_c(char *str)
+{
+char c;
+
+*str = str[0];
+while (*str != 'c')
+	{
+	*str++;
+		if (*str[c] == 'c')
+			{
+			write(1, "c\n", 2);
+			break;
+			}
+
+	}
+		
+ write(1, "c\n", 2);
+		
+}
+  
+int main(void)
+{
+  void aff_c(char *str);
+  aff_c("abcd");
+  return (0);
+}
